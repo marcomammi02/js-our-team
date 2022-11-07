b6dfe31c32544e3af95fb525e451283f4c107455
marcomammi02 <marcomammi2002@gmail.com>
2022-11-07 14:55:20 +0100

    Team members array

---
+++ script.js
@@ -0,0 +1,33 @@
+const arrProfiles = [
+    {
+        name: 'Wayne Barnett',
+        occupation: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',
+        occupation: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        occupation: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        occupation: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        occupation: 'Developer',
+        img: 'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramons',
+        occupation: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
